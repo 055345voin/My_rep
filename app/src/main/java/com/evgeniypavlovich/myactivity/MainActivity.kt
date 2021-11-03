918a0f809d31cb56330e7fdf12d9ae7b3700d038
EvgeniyPavlovitsh <evgeniypavlovitsh@gmail.com>
2021-11-03 16:32:22 +0300

    poem

--- app/src/main/java/com/evgeniypavlovich/myactivity/MainActivity.kt
+++ app/src/main/java/com/evgeniypavlovich/myactivity/MainActivity.kt
@@ -6,41 +6,64 @@ import android.util.Log
 
 private const val TAG = "MainActivity"
 private const val My_Own_Log_TAG = "MyOwnLog"
+var a = 0 // перебор ячеек массива
 
+fun poem() {
+    val linesOfAPoem = arrayOf(
+        "Ты видел деву на скале",
+        "В одежде белой над волнами",
+        "Когда, бушуя в бурной мгле,",
+        "Играло море с берегами,",
+        "Когда луч молний озарял",
+        "Ее всечасно блеском алым",
+        "И ветер бился и летал",
+        "С ее летучим покрывалом?",
+        "Прекрасно море в бурной мгле",
+        "И небо в блесках без лазури;",
+        "Но верь мне: дева на скале",
+        "Прекрасней волн, небес и бури."
+    )
+    val c = linesOfAPoem.size// c = длина массива
+    if (a < c) {
+        Log.d(My_Own_Log_TAG, linesOfAPoem[a])
+        a++
+    } else a=0// для зацикливания, что бы не выскакивала ошибка
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(My_Own_Log_TAG, "onCreate()")
+        poem()
     }
+
     override fun onStart() {
         super.onStart()
-        Log.d(My_Own_Log_TAG, "onStart()")
+        poem()
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d(My_Own_Log_TAG, "onResume()")
+        poem()
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(My_Own_Log_TAG, "onPause()")
+        poem()
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d(My_Own_Log_TAG, "onStop()")
+        poem()
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d(My_Own_Log_TAG, "onRestart()")
+        poem()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(My_Own_Log_TAG, "onDestroy()")
+        poem()
     }
 }
