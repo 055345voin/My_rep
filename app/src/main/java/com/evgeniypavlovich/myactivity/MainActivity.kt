84f2e2bb77b2d1f5bee58dc64f6f73c061c6f5ce
EvgeniyPavlovitsh <evgeniypavlovitsh@gmail.com>
2021-11-05 12:39:16 +0300

    Poem что за чем идет

--- app/src/main/java/com/evgeniypavlovich/myactivity/MainActivity.kt
+++ app/src/main/java/com/evgeniypavlovich/myactivity/MainActivity.kt
@@ -1,28 +1,53 @@
 package com.evgeniypavlovich.myactivity
-
-import android.content.Intent
-import android.net.Uri
-import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
 
-private const val KEY = "HELLO_KEY"
+private const val TAG = "MainActivity"
+private const val My_Own_Log_TAG = "MyOwnLog"
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val nextButton: Button = findViewById(R.id.next_button)
+        Log.d(My_Own_Log_TAG, "Ты видел деву на скале")
+        Log.d(My_Own_Log_TAG, "В одежде белой над волнами")
+    }
 
-        val link = Uri.parse("https://mail.ru/")
-        val intent = Intent(Intent.ACTION_VIEW, link)
+    override fun onStart() {
+        super.onStart()
+        Log.d(My_Own_Log_TAG, "Когда, бушуя в бурной мгле,")
+        Log.d(My_Own_Log_TAG, "Играло море с берегами,")
+    }
 
-        nextButton.setOnClickListener {
-            startActivity(intent)
-        }
+    override fun onResume() {
+        super.onResume()
+        Log.d(My_Own_Log_TAG, "Когда луч молний озарял")
+        Log.d(My_Own_Log_TAG, "Ее всечасно блеском алым")
+    }
 
+    override fun onPause() {
+        super.onPause()
+        Log.d(My_Own_Log_TAG, "И ветер бился и летал")
+        Log.d(My_Own_Log_TAG, "С ее летучим покрывалом?")
+    }
 
+    override fun onStop() {
+        super.onStop()
+        Log.d(My_Own_Log_TAG, "Прекрасно море в бурной мгле")
+        Log.d(My_Own_Log_TAG, "И небо в блесках без лазури;")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(My_Own_Log_TAG, "Но верь мне: дева на скале")
+        Log.d(My_Own_Log_TAG, "Прекрасней волн, небес и бури.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(My_Own_Log_TAG, "Но верь мне: дева на скале")
+        Log.d(My_Own_Log_TAG, "Прекрасней волн, небес и бури.")
     }
 }
-
-
